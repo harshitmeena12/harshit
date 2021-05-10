684672acd886cfb28f805352ee60ab36bd5e148c
Harshit <harshit.meena@wingify.com>
2021-05-10 16:47:13 +0530

    fix(Editor): 9 commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-document.getElementById('bt').addEventListener('click', function() {
-    document.getElementById('hd').innerHTML += 'button Clicked';
+$('#bt').click(function() {
+    document.getElementById('hd').innerHTML = 'button Clicked';
 });
 
-document.getElementById('hd2').addEventListener('click', function() {
-    document.getElementById('hd').innerHTML += 'heading Clicked';
+$('#hd2').click(function() {
+    document.getElementById('hd').innerHTML = 'heading Clicked';
 });
+
+
+// document.getElementById('bt').addEventListener('click', function() {
+//     document.getElementById('hd').innerHTML += 'button Clicked';
+// });
+
+// document.getElementById('hd2').addEventListener('click', function() {
+//     document.getElementById('hd').innerHTML += 'heading Clicked';
+// });
